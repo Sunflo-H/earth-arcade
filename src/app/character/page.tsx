e81a52ee24931113a402cf884baf6a3346a20732
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-08-16 23:44:15 +0900

    #1 html - 완료

---
+++ src/app/character/page.tsx
@@ -0,0 +1,9 @@
+const CharacterPage = () => {
+  return (
+    <>
+      <div>인물 퀴즈</div>
+    </>
+  );
+};
+
+export default CharacterPage;
---
+++ src/components/header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div>트로트 인물 퀴즈</div>;
+};
+
+export default Header;
